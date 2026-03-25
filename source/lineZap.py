b3c29405634ee8938cee4c5be18b3c1df0331fdb
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2026-03-25 18:31:08 -0400

    adding `-x` days functionality

--- source/lineZap.py
+++ source/lineZap.py
@@ -6,39 +6,47 @@
 
 import sys
 import os
-from datetime import datetime
-
-today = datetime.now()
-myUser = os.getlogin()
-
-
+import re
+from datetime import datetime, timedelta
 
 myFile = os.path.expanduser(os.getenv('MYFILE'))
 
-# date formatting
-todayStandard = today.strftime("%Y-%m-%d %a %-I:%M%p")
-
-
 myInput = sys.argv[1]
 
-file_content = [ line for line in open(myFile)]
+# check if input starts with a negative day offset like -1, -2, etc.
+match = re.match(r'^(-\d+)\s+(.*)', myInput)
+if match:
+    offset = int(match.group(1))
+    myInput = match.group(2)
+    day = datetime.now() + timedelta(days=offset)
+    todayStandard = day.strftime("%Y-%m-%d %a") + " 11:59PM"
+else:
+    todayStandard = datetime.now().strftime("%Y-%m-%d %a %-I:%M%p")
 
+file_content = [line for line in open(myFile)]
 
-completeEntry='- **'+ todayStandard + '** ' + myInput
+completeEntry = '- **' + todayStandard + '** ' + myInput + '\n'
 
+# parse the date from the new entry to find the right insertion point
+entryDate = datetime.strptime(todayStandard.split(' ')[0], "%Y-%m-%d")
 
+# find the correct position (file is newest-first)
+insertPos = 0
+for i, line in enumerate(file_content):
+    lineMatch = re.match(r'^- \*\*(\d{4}-\d{2}-\d{2})\s', line)
+    if lineMatch:
+        lineDate = datetime.strptime(lineMatch.group(1), "%Y-%m-%d")
+        if lineDate <= entryDate:
+            insertPos = i
+            break
+    insertPos = i + 1  # if no dated line found yet, insert after this line
 
+file_content.insert(insertPos, completeEntry)
 
-        
-f = open(myFile,"w")
-f.write(completeEntry)
-f.write("\n")
+f = open(myFile, "w")
 for line in file_content:
-	f.write(line)
-    #f.write("\n")
-    
-  
-f.close() 
+    f.write(line)
+f.close()
 
 
 
